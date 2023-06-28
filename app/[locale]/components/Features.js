7f95e534ef0debbf647fe199a86eb67740c547dc
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2023-06-28 13:55:22 -0600

    compared layouts

--- app/[locale]/components/Features.js
+++ app/[locale]/components/Features.js
@@ -47,6 +47,124 @@ export default function Features({ dictionary }) {
                 </div>
 
             </div>
+
+            <button className="border rounded-lg w-full inline-flex justify-between text-2xl p-4" type="button" onClick={toggleFeatures}>
+              Features 
+              {down ? <ArrowDown /> : <ArrowUp />}
+          </button>
+          
+          <div id="featuresList" className="shadow w-full">
+              <ul className="py-2 text-gray-700 dark:text-gray-200 divide-y divide-current rounded-lg">
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.customTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.customContent}
+                          </span>
+                      </button>
+                  </li>
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.colorTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.colorContent}
+                          </span>
+                      </button>
+                  </li>
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.respTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.respContent}
+                          </span>
+                      </button>
+                  </li>
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.interTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.interContent}
+                          </span>
+                      </button>
+                  </li>
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.seoTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.seoContent}
+                          </span>
+                      </button>
+                  </li>
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.accesibTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.accesibContent}
+                          </span>
+                      </button>
+                  </li>
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.manageTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.manageContent}
+                          </span>
+                      </button>
+                  </li>
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.dbTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.dbContent}
+                          </span>
+                      </button>
+                  </li>
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.ecomTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.ecomContent}
+                          </span>
+                      </button>
+                  </li>
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.adsTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.adsContent}
+                          </span>
+                      </button>
+                  </li>
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.analTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.analContent}
+                          </span>
+                      </button>
+                  </li>
+                  <li>
+                      <button className="featBtn">
+                          {dictionary.Features.apiTitle}
+                          <ArrowRight />
+                          <span className="featDesc1">
+                              {dictionary.Features.apiContent}
+                          </span>
+                      </button>
+                  </li>
+              </ul>
+          </div>
         </div>
     );
 }
