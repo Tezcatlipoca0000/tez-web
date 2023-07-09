f6d3375660f62dd0fa16ed7573ef2a799e4c9ac2
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2023-07-09 13:23:26 -0600

    Begun Prices Component

--- app/[locale]/components/Prices.js
+++ app/[locale]/components/Prices.js
@@ -1,6 +1,64 @@
 export default function Prices({ dictionary }) {
+
+    const options = [];
+
+    const featuresList = Object.keys(dictionary.Features).map((key, idx) => {
+        return (
+            <tr key={key}>
+                <td>
+                    {key}
+                </td>
+                <td>
+                    option
+                </td>
+                <td>
+                    price
+                </td>
+            </tr>
+        );
+    });
+
+    const servicesList = Object.keys(dictionary.Services).map((key, idx) => {
+        return (
+            <tr key={key}>
+                <td>
+                    {key}
+                </td>
+                <td>
+                    option
+                </td>
+                <td>
+                    price
+                </td>
+            </tr>
+        );
+    });
+
     return (
         <>
+        <h2>Prices:</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        Feature / Service
+                    </th>
+                    <th>
+                        Option
+                    </th>
+                    <th>
+                        Aprox.
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {featuresList}
+                {servicesList}
+            </tbody>
+            <tfoot>
+
+            </tfoot>
+        </table>
         </>
     );
 }
