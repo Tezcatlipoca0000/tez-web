d375bd33fa89c4c82314147e734bdfe74c6f5cfc
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2023-07-15 12:50:47 -0600

    continue carousel

--- app/[locale]/components/Features.js
+++ app/[locale]/components/Features.js
@@ -59,19 +59,33 @@ export default function Features({ dictionary }) {
         }
     }
 
+    function prevCarousel() {
+        let newPosition = position === 0 ? Object.keys(dictionary.Features).length - 1 : position - 1;
+        console.log('oiiiii ---> positions', position, newPosition);
+        setPosition(newPosition);
+    }
+
+    function nextCarousel() {
+        let newPosition = position === Object.keys(dictionary.Features).length - 1 ? 0 : position + 1;
+        setPosition(newPosition);
+    }
+
     const carouselItems = Object.keys(dictionary.Features).map((key, idx) => {
         return (
-            <div className="w-full h-full flex flex-col justify-center items-center">
-                <div className={`absolute top-0 left-0 w-full h-full ${idx === 0 ? '' : 'hidden'}`}>
-                    <Image src={bgImages[idx]} alt={`${key} background image`} fill style={{objectFit: "cover"}} className={features[key] ? '' : 'blur-sm'} />
-                </div>
-                <h3 className={`z-10 text-4xl ${key === 'custom' ? '' : 'hidden'}`}>
-                    {dictionary.Features[key].title}
-                </h3>
-                <p className="z-10 featDesc text-justify mt-5">
-                    {dictionary.Features[key].content}
-                </p>
-            </div>
+            <>
+                {position === idx && 
+                
+                    <div className={`absolute top-0 left-0 w-full h-full`}>
+                        <Image src={bgImages[idx]} alt={`${key} background image`} fill style={{objectFit: "cover"}} className={features[key] ? '' : 'blur-sm'} />
+                    </div>
+                }
+                <h3 className={`z-10 text-4xl`}>
+                        {dictionary.Features[key].title}
+                    </h3>
+                    <p className="z-10 featDesc text-justify mt-5">
+                        {dictionary.Features[key].content}
+                    </p>
+            </>
         );
     });
 
@@ -113,10 +127,10 @@ export default function Features({ dictionary }) {
         <>
             {/* Carousel */}
             <div className="relative flex justify-center items-center w-10/12 bg-slate-900 h-56 overflow-hidden rounded-lg md:h-96">
-                <div className="relative w-full h-full">
+                <div className="relative w-full h-full flex flex-col justify-center items-center">
                     {carouselItems}
                 </div>
-                <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
+                <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={prevCarousel}>
                     <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                         <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
@@ -124,7 +138,7 @@ export default function Features({ dictionary }) {
                         <span className="sr-only">Previous</span>
                     </span>
                 </button>
-                <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
+                <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={nextCarousel}>
                     <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                         <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
