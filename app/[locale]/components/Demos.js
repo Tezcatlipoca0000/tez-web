402bedbb0d29369c29e1d52d1f7c814a4b370d80
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2023-07-20 16:35:27 -0600

    Begun Demos section

--- app/[locale]/components/Demos.js
+++ app/[locale]/components/Demos.js
@@ -1,3 +1,11 @@
 export default function Demos() {
-    
+    return (
+        <div className="w-full magicpattern flex justify-center items-center py-16 px-8 my-14">
+            <div className="w-full h-full flex flex-wrap justify-evenly items-center">
+                <div className="w-1/5 h-60 bg-slate-300 border border-neutral-950">
+                    Demo's Demo
+                </div>
+            </div>
+        </div>
+    );
 }
